d0d840c44813da0f8ef9e03e9ee9bfbbb79b61fc
HediehEshaghi <eshaghi.ce@gmail.com>
2021-02-06 00:50:38 +0330

    Upper bar and homes

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,26 @@ public:
     void Accident(RenderWindow &window, Manager frog, Manager cars[], int n_cars);
     int Drown(RenderWindow &window, Manager frog, Manager woods[], int n_woods);
     void Move(float v_x, float v_y);
+    int CheckHome(RenderWindow &window, Manager frog, Manager homes[], int n_homes);
 };
+int Manager::CheckHome(RenderWindow &window, Manager frog, Manager homes[], int n_homes)
+{
+    for (int i = 0; i < n_homes; i++)
+    {  
+        if (homes[i].Shape.getGlobalBounds().contains(frog.Shape.getPosition().x+1, frog.Shape.getPosition().y+1))
+        {
+            if (homes[i].Shape.getGlobalBounds().contains(frog.Shape.getPosition().x+49, frog.Shape.getPosition().y+49))
+            {
+                return i;
+            }
+        }
+    }
+    if (frog.Shape.getPosition().y<50 && frog.Shape.getPosition().y>=0) 
+    {
+        window.close();
+    }  
+    return -1;
+}
 void Manager::CreatSprite(string const &FileName, float X, float Y)
 {
     static int count = 0;
@@ -35,7 +54,6 @@ void Manager::Accident(RenderWindow &window, Manager frog, Manager cars[], int n
 }
 int Manager::Drown(RenderWindow &window, Manager frog, Manager woods[], int n_woods)
 {
-    int s=0;
     for (int i = 0; i < n_woods; i++)
     {  
         if (woods[i].Shape.getGlobalBounds().contains(frog.Shape.getPosition().x+1, frog.Shape.getPosition().y+1))
@@ -43,11 +61,10 @@ int Manager::Drown(RenderWindow &window, Manager frog, Manager woods[], int n_wo
             if (woods[i].Shape.getGlobalBounds().contains(frog.Shape.getPosition().x+49, frog.Shape.getPosition().y+49))
             {
                 return i;
-                s++;
             }
         }
     }
-    if (s==0 && frog.Shape.getPosition().y<300 && frog.Shape.getPosition().y>=50) 
+    if (frog.Shape.getPosition().y<300 && frog.Shape.getPosition().y>=50) 
     {
         window.close();
     }  
@@ -95,11 +112,22 @@ int main()
     wood2.Shape.setPosition(Vector2f(0.0, 200.0));
     wood3.CreatSprite("../Asset/Image/Wood.png", 150.0, 50.0);
     wood3.Shape.setPosition(Vector2f(00.0, 150.0));
-    wood4.CreatSprite("../Asset/Image/Wood.png", 60.0, 50.0);
+    wood4.CreatSprite("../Asset/Image/Wood.png", 130.0, 50.0);
     wood4.Shape.setPosition(Vector2f(700.0, 100.0));
-    wood5.CreatSprite("../Asset/Image/Wood.png", 70.0, 50.0);
+    wood5.CreatSprite("../Asset/Image/Wood.png", 140.0, 50.0);
     wood5.Shape.setPosition(Vector2f(0.0, 50.0));
 
+    Manager Home1 ,Home2 ,Home3 ,Home4 ,Home5;
+    Home1.CreatSprite("../Asset/Image/Home.png", 70.0, 50.0);
+    Home1.Shape.setPosition(Vector2f(15.0, 0.0));
+    Home2.CreatSprite("../Asset/Image/Home.png", 70.0, 50.0);
+    Home2.Shape.setPosition(Vector2f(165.0, 0.0));
+    Home3.CreatSprite("../Asset/Image/Home.png", 70.0, 50.0);
+    Home3.Shape.setPosition(Vector2f(315.0, 0.0));
+    Home4.CreatSprite("../Asset/Image/Home.png", 70.0, 50.0);
+    Home4.Shape.setPosition(Vector2f(465.0, 0.0));
+    Home5.CreatSprite("../Asset/Image/Home.png", 70.0, 50.0);
+    Home5.Shape.setPosition(Vector2f(615.0, 0.0));
     while (window.isOpen())
     {
         sf::Event event;
@@ -134,7 +162,7 @@ int main()
             }
         }
         float velocity_cars[] = {-0.5,0.2,-0.5,0.2,-0.5};
-        float velocity_woods[] = {-0.1,0.1,0.5,-0.2,0.5};
+        float velocity_woods[] = {-0.1,0.1,0.1,-0.1,0.1};
         car1.Move(velocity_cars[0], 0.0);
         car2.Move(velocity_cars[1], 0.0);
         car3.Move(velocity_cars[2], 0.0);
@@ -146,7 +174,7 @@ int main()
         wood4.Move(velocity_woods[3], 0.0);
         wood5.Move(velocity_woods[4], 0.0);
 
-        //accident and drown
+        //accident, drown and home
         int n_cars = 5;
         Manager cars[]={car1,car2,car3,car4,truck};
         frog.Accident(window,frog,cars,n_cars);
@@ -155,16 +183,28 @@ int main()
         int wood_number = -1;
         wood_number = frog.Drown(window,frog,woods,n_woods);
         if (wood_number>-1) frog.Shape.move(velocity_woods[wood_number],0.0);
+        int n_homes=5;
+        Manager homes[]={Home1, Home2, Home3, Home4, Home5};
+        int home_number = -1;
+        home_number = frog.CheckHome(window,frog,homes,n_homes);
+        if (home_number>-1){
+            frog.Shape.setPosition(Vector2f(frog.Shape.getPosition().x, window.getSize().y-50 - frog.Shape.getSize().y));
+            if (home_number==0) Home1.CreatSprite("../Asset/Image/FrogWin.png", 70.0, 50.0);
+            else if(home_number==1) Home2.CreatSprite("../Asset/Image/FrogWin.png", 70.0, 50.0);
+            else if(home_number==2) Home3.CreatSprite("../Asset/Image/FrogWin.png", 70.0, 50.0);
+            else if(home_number==3) Home4.CreatSprite("../Asset/Image/FrogWin.png", 70.0, 50.0);
+            else if(home_number==4) Home5.CreatSprite("../Asset/Image/FrogWin.png", 70.0, 50.0);
+            
+        } 
         
-
         //update the game
         window.clear();
         window.draw(background.Shape);
         for (int i = 0; i < n_cars; i++) window.draw(cars[i].Shape);
         for (int i = 0; i < n_woods; i++) window.draw(woods[i].Shape);
         window.draw(frog.Shape);
+        for (int i = 0; i < n_homes; i++) window.draw(homes[i].Shape);
         window.display();
     }
-
     return 0;
 }
